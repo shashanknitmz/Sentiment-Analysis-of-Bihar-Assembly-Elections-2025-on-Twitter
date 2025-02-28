0b2656c97bb2199f59d8ccb415b24960e3e31f0f
Shashank Shekhar <shashanktara2612@gmail.com>
2025-02-28 12:41:24 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+api_key = "iE65t8Q09GBFRc3xoHEKbDVpm"
+api_secret = "zYjxzokvBNkwiKSObsYkbCLiBOoPtTdgQM60RCd13fXtf4Faai"
+access_token = "853506260284657666-wd9EL2uZSKvsgRk7j8oLcJUUnXVKlsu"
+access_secret = "tTHKOEfz9FIPQh0izaySC5haiQavSvBnG6BK1We6XsotS"
